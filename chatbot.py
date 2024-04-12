c82bf02316ba7c3576fbcd19e140cc4b3a80797e
nobody <lianpi3.ak@gmail.com>
2024-04-12 22:37:30 +0330

    ad response function and loop, cyber security wikipedia corpus

--- chatbot.py
+++ chatbot.py
@@ -39,7 +39,40 @@ def greet(sentence):
 
 
 #response generation
+def response(user_response):
+    robot_response=''
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    Tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(Tfidf[-1], Tfidf)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    reg_tfidf = flat[-2]
+    if(reg_tfidf==0):
+        robot_response= robot_response+"i'm sorry i don't understand your request"
+        return robot_response
+    else:
+        robot_response = robot_response+sent_tokens[idx]
+        return robot_response
+    
 
-
+#main running loop
+flag= True
+print("hello! how can i help you? if you want to exit just type exit any time")
+while(flag==True):
+    user_input= input()
+    user_input= user_input.lower()
+    if user_input != "exit":
+        if greet(user_input)!=None:
+            print("bot: "+greet(user_input))
+        else:
+            sent_tokens.append(user_input)
+            word_tokens=word_tokens+nltk.word_tokenize(user_input)
+            final_word= list(set(word_tokens))
+            print("bot: "+response(user_input))
+            sent_tokens.remove(user_input)
+    else: 
+        flag=False
+        print("goodbye, take care!")
 
 
